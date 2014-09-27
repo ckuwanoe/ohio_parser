0412ab40d2015b6d04ee888409ad586bccda72cf
Corey Kuwanoe <eschalon@gmail.com>
2014-09-26 19:52:21 -0700

    fixing van wert

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -5,7 +5,7 @@ class Voter < ActiveRecord::Base
   STANDARD_WITH_PHONE = {:county_voter_id => 7, :av_requested_date => 21, :av_sent_date => 14, :av_returned_date => 16, :ev_date => 16}
   NON_STANDARD = {"Athens" => STANDARD_WITH_PHONE, "Highland" => STANDARD_WITH_PHONE, "Jackson" => STANDARD_WITH_PHONE,
     "Marion" => STANDARD_WITH_PHONE, "Monroe" => STANDARD_WITH_PHONE, "Perry" => STANDARD_WITH_PHONE, "Putnam" => STANDARD_WITH_PHONE,
-    "Fulton" => STANDARD_WITH_PHONE, "Paulding" => STANDARD_WITH_PHONE, "VanWert" => STANDARD_WITH_PHONE}
+    "Fulton" => STANDARD_WITH_PHONE, "Paulding" => STANDARD_WITH_PHONE, "Van Wert" => STANDARD_WITH_PHONE}
   EV = %w( INPERSON PERSON EARLY )
 
   def self.parse_csv(file)
